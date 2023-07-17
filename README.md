79248d128b1f01b5f52ff1e8e2c5ae8d2b822690
hyunsu00 <hyun955807@naver.com>
2023-07-18 00:00:58 +0900

    코드추가

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mongooseWebServer
+
+```bash
+# packed_fs.c 만들기
+pack public/index.html public/HwpTextExtractor.js public/HwpTextExtractor.wasm public/samples/한글문서.hwp > packed_fs.c
+```
+
+> [!WARNING]
+> 영어이외의 언어 파일은 인식에 실패한다.
---
+++ mongooseWebServer/main.c
@@ -0,0 +1,28 @@
+﻿// main.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "mongoose.h"
+
+const char *s_listening_url = "http://0.0.0.0:8000";
+
+// HTTP request handler function
+void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+	if (ev == MG_EV_HTTP_MSG) {
+		struct mg_http_serve_opts opts = {
+			.root_dir = "/public",
+			.fs = &mg_fs_packed
+		};
+		mg_http_serve_dir(c, ev_data, &opts);
+	}
+	(void)fn_data;
+}
+
+int main()
+{
+	struct mg_mgr mgr;
+	mg_log_set(MG_LL_INFO);
+	mg_mgr_init(&mgr);
+	mg_http_listen(&mgr, s_listening_url, fn, NULL);
+	while (true) mg_mgr_poll(&mgr, 500);
+	mg_mgr_free(&mgr);
+	return 0;
+}
